01ec089a4d70ec35af9ca7f73472400db312135d
huangyu4026-create <huangyu4026@gmail.com>
2026-06-28 17:02:08 +0100

    Refresh service worker for sync13

--- work-life-mobile/sw.js
+++ work-life-mobile/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "work-life-mobile-standalone-v12";
+const CACHE_NAME = "work-life-mobile-standalone-v13";
 const ASSETS = [
   "./",
   "./index.html",
-  "./cloud-config.js?v=20260628-sync12",
+  "./cloud-config.js?v=20260628-sync13",
   "./manifest.webmanifest",
   "./icon.svg",
   "./original-work-cockpit-state.json"
