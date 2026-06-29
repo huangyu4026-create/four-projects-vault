b692aa4080bb13ae804b12c15a6093991be37fff
huangyu4026-create <huangyu4026@gmail.com>
2026-06-29 07:26:23 +0100

    Bump mobile cache to sync26

--- work-life-mobile/sw.js
+++ work-life-mobile/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "work-life-mobile-standalone-v25";
+const CACHE_NAME = "work-life-mobile-standalone-v26";
 const ASSETS = [
   "./",
   "./index.html",
-  "./cloud-config.js?v=20260629-sync25",
+  "./cloud-config.js?v=20260629-sync26",
   "./manifest.webmanifest",
   "./icon.svg",
   "./original-work-cockpit-state.json"
