ac045992a52d31481be514b0a192ecfa3b56e851
yu <yu@545deMac-mini.local>
2026-07-06 13:52:34 +0100

    Connect digital life public entry to Apps Script inbox

--- digital-life-public/cloud-config.js
+++ digital-life-public/cloud-config.js
@@ -1,18 +1,4 @@
-(function () {
-  const APP_SCRIPT_URLS = [
-    "https://script.google.com/macros/s/AKfycbz8MaiJ8h9nfCp0VEXmC0a0WxZ17W7REx_9PtRAP9EU39DWax3V-4Jc-vKwskRO5Wk/exec",
-    "https://script.google.com/macros/s/AKfycbxNpxYB2dnufuahisV5PMD-8GbnoHbazsmZsr-SgBLx1-vKD3FBGwQeBdDm9VwEjVt8/exec"
-  ];
-  const APP_SCRIPT_TOKEN = "";
-
-  const APP_SCRIPT_URL = APP_SCRIPT_URLS[0];
-  window.DIGITAL_LIFE_PUBLIC_INBOX_URL = APP_SCRIPT_URL;
-  window.DIGITAL_LIFE_PUBLIC_INBOX_URLS = APP_SCRIPT_URLS;
-  window.DIGITAL_LIFE_PUBLIC_TOKEN = APP_SCRIPT_TOKEN;
-
-  try {
-    const params = new URLSearchParams(location.search);
-    if (params.get("cloud")) window.DIGITAL_LIFE_PUBLIC_INBOX_URL = params.get("cloud");
-    if (params.get("token")) window.DIGITAL_LIFE_PUBLIC_TOKEN = params.get("token");
-  } catch (_) {}
-})();
+// 数字生命公网今日日志入口云端配置。
+// 只放 Google Apps Script Web App /exec 地址；不要在公网放 OpenAI API key。
+window.DIGITAL_LIFE_PUBLIC_INBOX_URL = "https://script.google.com/macros/s/AKfycbz8MaiJ8h9nfCp0VEXmC0a0WxZ17W7REx_9PtRAP9EU39DWax3V-4Jc-vKwskRO5Wk/exec";
+window.DIGITAL_LIFE_PUBLIC_TOKEN = "";
